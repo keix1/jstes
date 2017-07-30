8d05791f8cfcf9eee86c8ec1a8084415e5e3187c
ke_ix1 <xkei4dev@gmail.com>
2017-07-30 23:18:00 +0900

    bbs.ejsを追加しポストした値を表示できる

--- dotes/server.js
+++ dotes/server.js
@@ -1,21 +1,40 @@
+
 var http = require('http'),
-    fs = require('fs');
+    fs = require('fs'), //ファイルシステム操作
+    ejs = require('ejs'), // js的html
+    qs = require('querystring'); //
+
 var server = http.createServer();
-var settings = require('./settings.js');
-console.log(settings);
+var settings = require('./settings.js'); //ここにip等の設定書く
+console.log(settings + ' : ' + __dirname);
+var posts = [];
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+
+function renderForm(posts, res) {
+    var data = ejs.render(template, {
+        posts: posts
+    }); // 読み込んだテンプレートに入れ込む
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+
+}
+
 server.on('request', function(req, res) {
-    fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
-        if(err) {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write('not found!');
-            return res.end();
-        }
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-
-    });
+    if(req.method==='POST') {
+        req.data = "";
+        req.on("data", function(chunk) {
+            req.data += chunk;
+        });
+        req.on("end", function() {
+            var query = qs.parse(req.data);
+            posts.push(query.name);
+            renderForm(posts,res);
+        });
+    } else {
+        renderForm(posts, res);
+    }
 
 });
 server.listen(settings.port, settings.host);
